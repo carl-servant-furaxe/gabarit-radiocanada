d6e53040c959cc9b04bc3fbbe2303fa125c267af
sequencedigitale <info@sequencedigitale.com>
2019-07-04 10:48:54 -0400

    Premier depot

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr" class="nojs" data-form-state="!closed">
+<head>
+
+
+
+<!-- CODE :: Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-87186208-51&api=1"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-87186208-51', { 'anonymize_ip': true });
+</script>
+<!-- /CODE :: Google Analytics -->
+
+
+	<meta charset="utf-8" />
+	<meta name="robots" content="index, follow"/>
+	<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1, user-scalable=no" />
+	<script src="https://assets.adobedtm.com/2eda04f28b1fa2bbcd3ec449dfdc174232ed3359/satelliteLib-74306c976638b05ba3f239cf5faa790c1a5b5fda.js"></script>
+	<meta property="og:site_name" content="Radio-Canada" />
+	<meta name="Copyright" content="Société Radio-Canada" />
+	<meta name="dc.publisher" content="ICI Radio-Canada" />
+	<meta name="dc.creator" content="Zone @@@4 - ICI Radio-Canada.ca" />
+	<meta name="dc.date.created" content="2015-07-17" />
+	<meta name="dc.date.modified" content="2015-07-17" />
+	<meta name="dc.language" content="fr-ca" />
+
+
+
+<!-- CODE :: Meta -->
+<title>@@@1 | ICI Radio-Canada.ca</title>
+<meta name="Description" content="@@@3b" />
+<meta name="Keywords" content="@@@3a" />
+<meta name="dc.title" content="@@@1 | ICI Radio-Canada.ca" />
+<meta name="dc.keywords" content="@@@3a" />
+<meta name="dc.description" content="@@@3b" />
+<meta property="og:url" content="@@@4a" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="@@@1 | ICI Radio-Canada.ca" />
+<meta property="og:description" content="@@@3b" />
+<meta property="og:image" content="@@@4b" />
+<!-- /CODE :: Meta -->
+<!-- CODE :: Omniture -->
+<meta id="metaWebTrends" name="WT.sp" content="global;@@@5" />
+<meta name="WT.clic" content="" />
+<meta name="rc.domaine" content="" />
+<meta name="rc.application" content="" />
+<meta name="rc.formatApplication" content="" />
+<meta name="rc.section" content="" />
+<meta name="rc.groupeSection" content="" />
+<meta name="rc.segment" content="" />
+<meta name="rc.pageSegment" content="" />
+<meta name="rc.codePage" content="" />
+<meta name="rc.niveau" content="" />
+<meta name="rc.titre" content="" />
+<meta name="rc.typeDocument" content="" />
+<meta name="rc.typeContenu" content="" />
+<meta name="rc.theme" content="" />
+<meta name="rc.sousTheme" content="" />
+<meta name="rc.dateCreation" content="" />
+<meta name="rc.region" content="" />
+<meta name="rc.blogue" content="" />
+<meta name="rc.sport" content="" />
+<meta name="rc.emission" content="" />
+<meta name="rc.reseau" content="" />
+<meta name="rc.chaine" content="" />
+<meta name="rc.saison" content="" />
+<!-- /CODE :: Omniture -->
+
+
+
+	<link href="https://ici.radio-canada.ca/content/generated/main.min.css?v=1.0.0.26060" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="https://ici.radio-canada.ca/Content/component.DynamicMenu.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="https://ici.radio-canada.ca/Content/component.StickyNav.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="https://ici.radio-canada.ca/Content/component.Menuzone.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="https://ici.radio-canada.ca/Content/component.Footer.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="https://s.radio-canada.ca/_css/modules/modules.barrepartagee.1.0.0.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="https://s.radio-canada.ca/mp/modules.socialbar.1.0.0.css" />
+
+	<script>
+	window.appHost = 'https://ici.radio-canada.ca';
+	</script>
+
+	<script src='https://ici.radio-canada.ca/Content/generated/main.min.js'></script>
+	<script src='https://ici.radio-canada.ca/Content/generated/app.min.js'></script>
+
+	<script src='https://geoip.radio-canada.ca/geoip.js'></script>
+	<script src='https://s.radio-canada.ca/_js/modules/modules.pub.manuel.js'></script>
+	<script src='https://s.radio-canada.ca/_js/modules/pub.fixnav.js'></script>
+	<script type="text/javascript" src="https://s.radio-canada.ca/mp/viafoura.js"></script>
+	<script type="text/javascript" src="https://s.radio-canada.ca/mp/modules.socialbar.executer.6.0.js"></script>
+
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+	<script type="text/javascript"><!--
+	var html = document.getElementsByTagName('html')[0];
+	html.className = html.className.replace('nojs', 'js');
+	--></script>
+
+
+
+<!-- CODE :: Stylesheets -->
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800,300italic,400italic,600italic,700italic,800italic|Roboto:300,400,700|Roboto+Condensed:300,400,500,600,700' rel='stylesheet' type='text/css' />
+<link rel="stylesheet" href="assets/css/style.css?<?php echo filemtime('assets/css/style.css'); ?>" />
+<!-- /CODE :: Stylesheets -->
+
+
+
+</head>
+<body class="screen-medium">
+
+
+
+<!-- CODE :: Pixel de reciblage -->
+<div id="pixel">
+	<img src="http://lien-vers-le-pixel.jpg" alt="" height="1" width="1" />
+</div>
+<!-- /CODE :: Pixel de reciblage -->
+
+
+
+<script src='https://s.radio-canada.ca/omniture/omni_stats_base.js?version=2014110501'></script>
+<div id="hostPubBanner"></div><div id="hostNavSticky"></div><div id="hostNavigation"></div><div id="hostNavigationZone"></div>
+<div id="src-container">
+	<div id="src-content" class="clearfix" tabindex="0">
+
+
+
+<!-- CODE :: sponsored -->
+<div id="sponsored"><div class="inner">Contenu commandité</div></div>
+<!-- /CODE :: sponsored -->
+
+
+
+<!-- CODE :: Debut du Microsite-->
+<div id="wrapper">
+
+
+</div>
+<!-- CODE :: Fin du Microsite-->
+
+
+
+	</div>
+</div>
+<div id="hostFooter"></div>
+<script>
+var section = 'tele';
+function loadAdvertising() {
+	bunkerRequire(['underscore', 'services/componentsService', 'externals/components/adsmanager/main'], function(_, cpSvc, adsmanager) {
+		adsmanager.init();
+		adsmanager.processInit();
+	});
+}
+
+bunkerRequire(['jquery', 'services/componentsService', 'underscore'], function(jq, cpSvc, _) {
+	var navAsync = cpSvc.load('dynamicmenu', { hideleaderboard: true }, document.getElementById('hostNavigation'));
+	var footerAsync = cpSvc.load('footer', {}, document.getElementById('hostFooter'));
+	var stickyNavAsync = cpSvc.load('StickyNav', { itemSelector: '#hostNavigation', url: section }, document.getElementById('hostNavSticky'));
+	var bannerAsync = cpSvc.load('banner', {}, document.getElementById('hostPubBanner'));
+	if (section === 'tele') {
+		var navZoneTeleASync = cpSvc.load('menuZone', {}, document.getElementById('hostNavigationZone'));
+	}
+	var bigBoxAsync = cpSvc.load('bigbox', { external: true }, document.getElementById('hostPubBigBox'));
+	jq.when(navAsync, footerAsync, stickyNavAsync, bannerAsync, bigBoxAsync).done(function(nav, footer, sticky, banner, bigbox) {
+		_.delay(loadAdvertising, 500);
+	});
+});
+</script>
+<script type="text/javascript">_satellite.pageBottom();</script>
+
+
+
+<!-- CODE :: Javascripts -->
+<script type="text/javascript" src="assets/js/lightbox.js?<?php echo filemtime('assets/js/lightbox.js'); ?>"></script>
+<script type="text/javascript" src="assets/js/share.js?<?php echo filemtime('assets/js/share.js'); ?>"></script>
+<!-- /CODE :: Javascripts -->
+
+
+
+</body>
+</html>
